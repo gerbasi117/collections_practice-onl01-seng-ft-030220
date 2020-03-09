44b7a938b40fba70ca3275ff1b5a0fcc7a2babd0
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-09 02:52:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,14 +3,19 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do | left, right|
-    right <=> left
+  array.sort do | one, two|
+    one <=> two
   end
 end
 
 def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
+  array.sort do |one, two|
+    one.length <=> two.length
   end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
